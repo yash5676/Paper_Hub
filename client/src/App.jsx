c16c50b40b367cbbef56a59b06f747cc31985038
yash5676 <yashkumargoyal1804@gmail.com>
2024-02-01 18:39:29 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,25 @@
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import Homepage from "./pages/Homepage";
+
+
+
+
+import './App.css';
+
+export default function App() {
+  return (
+   <>
+   <BrowserRouter>
+   <Routes>
+   <Route
+   path="/"
+   element={
+  <Homepage/>
+    
+   }
+   />
+   </Routes>
+   </BrowserRouter>
+   </>
+  )
+}
---
+++ client/src/pages/Homepage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import {Hero} from "../components"
+
+const Homepage=()=>{
+return (
+  <>
+  <Hero/>
+  </>
+)
+
+}
+
+export default Homepage
